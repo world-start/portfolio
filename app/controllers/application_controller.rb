81562f2293ae813f2c67cac9788151369c87a75e
world-start <23fn40@gmail.com>
2021-10-29 23:31:48 +0000

    Add3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,24 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin #Adminモデル
+      admin_root_path #遷移先のパス
+    when User
+      root_path
+    end
+  end
 
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      new_admin_session_path
+    else
+      root_path
+    end
+  end
+
+  protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
