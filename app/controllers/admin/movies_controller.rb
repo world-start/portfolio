6480007b0307efb0d0e45d9c721401b150dd12ed
world-start <23fn40@gmail.com>
2021-10-31 10:55:34 +0000

    Add

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -1,10 +1,16 @@
 class Admin::MoviesController < ApplicationController
   def new
+    @movie = Movie.new
   end
 
   def create
   end
 
   def destroy
+  end
+
+  private
+  def list_params
+    params.require(:movie).permit(:review_id, :title, :image, :introduction, :created_year, :director, :cast)
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,4 @@
 class Movie < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  attachment :image
 end
